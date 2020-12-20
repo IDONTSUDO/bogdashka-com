8aa0fc3bde3f1a45e639bd143ed8509909bb0fea
IDONTSUDO <idontsudo@gmail.com>
2020-12-20 22:51:11 +0300

    new

--- test/http.test.ts
+++ test/http.test.ts
@@ -1,22 +1,18 @@
-
 import { expect } from 'chai';
 import { Group } from '../src/model/Group';
 import axios from 'axios';
 import * as Mock from './mocks/httpTransaction.json';
 import * as env from '../src/config/env.json';
 
-let server = env.serverURL;
+let server = 'http://localhost:8080/';
 let mockId = '1dbc8680-42f4-11eb-85d0-17d057c3393f'
  
 describe('Тесты HTTP сервера', () => {
     it('Тест перевода PEDDING', async () => {
-        const p = await axios.post('http://localhost:8080/qiwi/pay', Mock)
+        const p = await axios.post(`${server}qiwi/pay`, Mock);
         expect(p.data).to.be.an('string');
     });
     it('Тест перевода COMPLETE',async () => {
-        const p = await axios.post(`http://localhost:8080/qiwi/complete/?id=${mockId}`)
-        // console.log(p);
-
+        await axios.post(`${server}qiwi/complete/?id=${mockId}`);
     })
 }); 
-// /path/filename?id=123&option=456
