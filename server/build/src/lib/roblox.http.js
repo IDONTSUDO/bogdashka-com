98af6c79dea321a467622f5f0e2cffb13c5d5016
IDONTSUDO <idontsudo@gmail.com>
2020-12-26 22:04:07 +0300

    server change

--- server/build/src/lib/roblox.http.js
+++ server/build/src/lib/roblox.http.js
@@ -17,21 +17,22 @@ class RobloxApi {
      * @static
      * @param {Кука} cookies
      * @param {Айди группы} groupId
-     * @param {} body
+     * @param {сколько отослать} amount
+     * @param {айди юзера} userId
      * @return {*}
      * @memberof RobloxApi
      */
-    static async transaction(cookies, groupId) {
-        const sessssionTokenCache = this.getXCrfToken(cookies, groupId);
+    static async transaction(cookies, groupId, amount, userId) {
+        const sessssionTokenCache = this.getXCrfToken(cookies);
         try {
+            const resBody = [{ 'recipientId': userId, 'recipientType': 'User', 'amount': amount }];
             const p = await axios_1.default.post(`${roblox_urlV1}groups/${groupId}/payouts`, {
-                body: [{ "recipientId": 727236058, "recipientType": "User", "amount": 1 }],
+                body: JSON.stringify(resBody),
                 headers: {
                     cookie: cookies,
                     'x-csrf-token': sessssionTokenCache
                 }
             });
-            console.log(p.data);
             return p;
         }
         catch (error) {
@@ -45,11 +46,11 @@ class RobloxApi {
      * @return {*}  {(Promise<String | void>)}
      * @memberof RobloxApi
      */
-    static async getXCrfToken(cookies, groupId) {
+    static async getXCrfToken(cookies) {
         try {
-            const p = await axios_1.default.get(`${roblox_urlMain}/home`, { headers: { 'cookie': cookies } });
-            let result = p.data.match(/setToken.*'(\w+)'/);
-            return result[1];
+            const p = await axios_1.default.get(`https://web.roblox.com/home`, { headers: { 'cookie': cookies } });
+            let result = resolveTokenStr(p.data);
+            return result;
         }
         catch (error) {
             console.log(error);
@@ -63,7 +64,7 @@ class RobloxApi {
      * @memberof RobloxApi
      */
     static async getGroupBalance(groupId, cookies) {
-        const sessssionTokenCache = this.getXCrfToken(cookies, groupId);
+        const sessssionTokenCache = this.getXCrfToken(cookies);
         const res = await axios_1.default.get(`https://economy.roblox.com/v1/groups/${groupId}/currency`, { headers: { 'cookie': cookies, 'x-csrf-token': sessssionTokenCache } });
         const data = res.data;
         const { robux } = data;
@@ -78,7 +79,7 @@ class RobloxApi {
      */
     static async UserLoginWithGroup(user, cookies, groupId) {
         try {
-            const sessssionTokenCache = this.getXCrfToken(cookies, groupId);
+            const sessssionTokenCache = this.getXCrfToken(cookies);
             const res = await axios_1.default.get(`https://users.roblox.com/v1/users/search?keyword=${user}&limit=10}`, { headers: { 'cookie': cookies, 'x-csrf-token': sessssionTokenCache } });
             const data = res.data;
             if (data.data.length != 0) {
@@ -99,4 +100,27 @@ class RobloxApi {
     }
 }
 exports.RobloxApi = RobloxApi;
+function resolveTokenStr(data) {
+    const reg = new RegExp(/setToken.*'(\w+)'/);
+    const index = data.search(reg);
+    let rangeMin;
+    let rangeMax;
+    index + 9;
+    let i = index;
+    for (i; i < data.length; i++) {
+        if (data[i] === `'`) {
+            if (rangeMax === undefined) {
+                if (rangeMin === undefined) {
+                    rangeMin = i + 1;
+                }
+                else {
+                    rangeMax = i;
+                }
+            }
+        }
+    }
+    console.log(rangeMin, rangeMax);
+    // console.log(data.slice(rangeMin,rangeMax))
+    return data.slice(rangeMin, rangeMax);
+}
 //# sourceMappingURL=roblox.http.js.map
